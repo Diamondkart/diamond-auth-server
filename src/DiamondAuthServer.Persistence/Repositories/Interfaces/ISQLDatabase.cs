3f8584b9984e91097eedccfd992128b6f2f34532
Prakash Besra <Prakash.Besra@parentpay.com>
2023-12-20 00:23:54 +0530

    [Prakash]: Build db connection builder

---
+++ src/DiamondAuthServer.Persistence/Repositories/Interfaces/ISQLDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondAuthServer.Persistence.Repositories.Interfaces
+{
+    public interface ISQLDatabase
+    {
+        IDbConnection GetConnection();
+    }
+}
---
+++ src/DiamondAuthServer.Persistence/Repositories/SQLDatabase.cs
@@ -0,0 +1,31 @@
+﻿using DiamondAuthServer.Persistence.Repositories.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace DiamondAuthServer.Persistence.Repositories
+{
+    public class SQLDatabase : ISQLDatabase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SQLDatabase(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return new SqlConnection(GetConnectionString());
+        }
+
+        private string GetConnectionString()
+        {
+            var sqlConnection = new SqlConnectionStringBuilder
+            {
+                TrustServerCertificate = true
+            };
+            return sqlConnection.ConnectionString;
+        }
+    }
+}
